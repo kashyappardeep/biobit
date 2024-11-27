3febf4092e5b58e98168abd6c02dc36283c31bac
kashyappardeep <pkk41334@gmail.com>
2024-11-27 15:28:58 +0530

    hogo change

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,10 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        {{-- <script src="{{asset('assets/js/app.js')}}"></script>
-        <script src="{{asset('assets/js/bootstrap.js')}}"></script>
-        <link href="{{asset('assets/css/app.css')}}" rel="stylesheet"> --}}
-        <!-- Scripts -->
+       
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased" >
