9a4ea5e5e6a341aa26b832e9e480e84649d0c974
kashyappardeep <pkk41334@gmail.com>
2024-12-04 12:50:37 +0530

    reffreal link copy error resoleve

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,34 +5,4 @@
 </body>
 
 </html>
-<script>
-    function copyReferralLink() {
-        // Get the referral link text
-        console.log('Function is called');
-        const referralLink = document.getElementById("referralLink").innerText;
 
-        // Copy the text to the clipboard
-        navigator.clipboard.writeText(referralLink)
-            .then(() => {
-                // SweetAlert2 Success Message
-                Swal.fire({
-                    title: "Success!",
-                    text: "Referral link copied",
-                    icon: "success",
-                    timer: 2000, // Set timer for 2 seconds
-                    timerProgressBar: true,
-                    showConfirmButton: false 
-                });
-            })
-            .catch(err => {
-                console.error("Failed to copy referral link: ", err);
-                // SweetAlert2 Error Message
-                Swal.fire({
-                    title: "Error!",
-                    text: "Failed to copy referral link.",
-                    icon: "error",
-                    confirmButtonText: "OK"
-                });
-            });
-    }
-</script>
